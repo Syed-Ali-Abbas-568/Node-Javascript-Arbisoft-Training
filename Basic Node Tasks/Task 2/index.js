1c568646333f0e626dd66f9be28109479a0d6d5b
Syed-Ali-Abbas-568 <ali.abbas02@arbisoft.com>
2024-09-12 10:38:51 +0500

    Commited NodeJS Task2

---
+++ Basic Node Tasks/Task 2/index.js
@@ -0,0 +1,7 @@
+import { add,subtract,multiply,divide } from "./calculator.js";
+
+console.log('Addition:', add(5, 3));
+console.log('Subtraction:', subtract(10, 4));
+console.log('Multiplication:', multiply(6, 2));
+console.log('Division:', divide(15, 3));
+console.log('Division by zero:', divide(10, 0));
