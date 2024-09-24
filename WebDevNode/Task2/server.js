263a33972e6413860fb0438c60772574052164a2
Syed-Ali-Abbas-568 <ali.abbas02@arbisoft.com>
2024-09-24 09:41:03 +0500

    Completed WebDevNode Task2, Task3 & Task4

---
+++ WebDevNode/Task2/server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import routes from './routes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(express.static('uploads'));
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
+// File upload configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedFileTypes = /jpeg|jpg|png|pdf/;
+  const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = allowedFileTypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Only .pdf, .jpg, .jpeg, and .png files are allowed!');
+  }
+};
+
+
+// Routes
+app.use('/', routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!', error: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
